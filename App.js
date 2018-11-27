d7c65261b714fa661e94d9170f2a305b4c686b90
Lodavier <lodavier@iMac.local>
2018-11-27 20:29:42 +0000

    platform select

--- App.js
+++ App.js
@@ -9,20 +9,18 @@
 import React, {Component} from 'react';
 import {Platform, StyleSheet, Text, View, Button, TextInput, Picker, Slider, Switch} from 'react-native';
 
-const instructions = Platform.select({
-  ios: 'Press Cmd+R to reload,\n' + 'Cmd+D or shake for dev menu',
-  android:
-    'Double tap R on your keyboard to reload,\n' +
-    'Shake or press menu button for dev menu',
-});
-
 type Props = {};
 export default class App extends Component<Props> {
   constructor(props){
     super(props);
 
+    const defaultPlatform = Platform.select({
+      ios: "ios",
+      android: "android"
+    });
+
     this.state = {
-      platform: "android",
+      platform: defaultPlatform,
       text: "",
       slider: 15,
       switch: false
